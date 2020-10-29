21f50cc81ad5ebbd3d46948ad4acb5f3586f4b9d
Jonny <jonny.lie@student.uts.edu.au>
2020-10-29 22:43:40 +1100

    production

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const invalidJson = { "error": "Could not decode request: JSON parsing failed" }
 app.use(express.json({
     verify: (req, res, buf, encoding) => {
         try {
-            JSON.parse(buf);
+            JSON.parse(buf); // if it parses invalidate JSON data, return a json response with an error message
         } catch (e) {
             res.status(400).send(invalidJson);
             throw Error('invalid JSON');
@@ -19,7 +19,7 @@ app.post('/', (req, res) => {
     let arrOfObjects = new Object();
     let result = new Array;
 
-    if (req.body.hasOwnProperty('payload')) {
+    if (req.body.hasOwnProperty('payload')) { // if JSON data has payload as a key, grab the value. Otherwise, return an error
         arrOfObjects = req.body.payload;
     } else {
         res.status(400).send(invalidJson);
@@ -31,18 +31,18 @@ app.post('/', (req, res) => {
         let show = {};
 
         if (obj.drm === true && obj.episodeCount > 0) {
-            const hasAllProperties = checkAllProperties(obj, 'image', 'slug', 'title');
-            if (hasAllProperties) {
+            const hasAllProperties = checkAllProperties(obj, 'image', 'slug', 'title'); // check whether we have all of three keys in each object
+            if (hasAllProperties) { // if we have all of them, assign them to a newly created object
                 show.image = obj.image.showImage;
                 show.slug = obj.slug;
                 show.title = obj.title;
-                result.push(show);
+                result.push(show);// push the object to an array
             }
         }
 
     }
     let json = new Object();
-    json.response = result;
+    json.response = result; // finally, add key value pair to a JSON
     res.send(json);
 });
 
@@ -54,5 +54,5 @@ const checkAllProperties = (object, image, slug, title) => {
 }
 
 module.exports = app;
-// app.listen(3000, () => console.log('Listening on Port 3000'));
+
 
